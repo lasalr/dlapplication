c85bfe0e69e3dd973137ce2f9bf27bd062a51475
Lasal Ranasinghe <lran0009@student.monash.edu>
2020-07-24 20:40:43 +1000

    Created get_sklearn_sv_classification function

--- dlutils/models/sklearn/classification.py
+++ dlutils/models/sklearn/classification.py
@@ -3,3 +3,7 @@
         
         learner = LogisticRegression(regParam = regParam, dim = dim)
         return learner
+
+    def get_sklearn_sv_classification(self, regParam, dim):
+            # TODO Implement similar function to getSKLearnLogisticRegression()
+            pass
