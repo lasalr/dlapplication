c5be5cd35ef9da9da3810c6c98ad46be462f954f
Lasal Ranasinghe <lran0009@student.monash.edu>
2020-08-12 22:41:02 +1000

    Created SVM Experiment

---
+++ experiments/local_experiments/examples/averagingAtTheEnd/BatchLearningSVM.py
@@ -0,0 +1,36 @@
+import sys
+sys.path.append("../../../../../dlapplication")
+sys.path.append("../../../../../dlplatform")
+
+
+from environments.local_environment import Experiment
+from environments.datasources.standardDataSourceFactories import SVMLightDataSourceFactory
+from DLplatform.aggregating import Average
+from DLplatform.synchronizing.aggAtTheEnd import AggregationAtTheEnd
+from DLplatform.learning.factories.sklearnBatchLearnerFactory import SklearnBatchLearnerFactory
+from DLplatform.learning.batch.sklearnClassifiers import LogisticRegression
+from DLplatform.learning.batch.sklearnClassifiers import LinearSVC
+from DLplatform.stopping import MaxAmountExamples
+
+
+if __name__ == "__main__":
+  
+    messengerHost = 'localhost'
+    messengerPort = 5672
+    numberOfNodes = 3
+    
+    regParam = 0.1
+    dim = 4 #skin_segmentation has 4 attributes
+    learnerFactory = SklearnBatchLearnerFactory(LinearSVC, {'regParam' : regParam, 'dim' : dim})
+    
+    dsFactory = SVMLightDataSourceFactory("../../../../data/classification/skin_segmentation.dat", numberOfNodes, indices = 'roundRobin', shuffle = False)
+    stoppingCriterion = MaxAmountExamples(30000)
+        
+    aggregator = Average()
+    sync = AggregationAtTheEnd()
+    
+    exp = Experiment(executionMode = 'cpu', messengerHost = messengerHost, messengerPort = messengerPort, 
+        numberOfNodes = numberOfNodes, sync = sync, 
+        aggregator = aggregator, learnerFactory = learnerFactory, 
+        dataSourceFactory = dsFactory, stoppingCriterion = stoppingCriterion, sleepTime=0)
+    exp.run("RadonMachine_test")
