b657784fb29ce487846504997d8340c11d7ffe48
Lasal Ranasinghe <lran0009@student.monash.edu>
2020-08-27 11:13:11 +1000

    Changed experiment

--- experiments/local_experiments/examples/averagingAtTheEnd/BatchLearningLinearSVC.py
+++ experiments/local_experiments/examples/averagingAtTheEnd/BatchLearningLinearSVC.py
@@ -4,7 +4,8 @@ sys.path.append("../../../../../dlplatform")
 
 
 from environments.local_environment import Experiment
-from environments.datasources.standardDataSourceFactories import SVMLightDataSourceFactory
+from environments.datasources.standardDataSourceFactories import FileDataSourceFactory
+from environments.datasources.dataDecoders.otherDataDecoders import HIGGSDecoder
 from DLplatform.aggregating import Average
 from DLplatform.aggregating import RadonPoint
 from DLplatform.synchronizing.aggAtTheEnd import AggregationAtTheEnd
@@ -18,20 +19,27 @@ if __name__ == "__main__":
   
     messengerHost = 'localhost'
     messengerPort = 5672
-    numberOfNodes = 5
+    numberOfNodes = 4
     
-    regParam = 0.001
+    regParam = 0.01
     dim = 4 #skin_segmentation has 4 attributes
     learnerFactory = SklearnBatchLearnerFactory(LinearSVC, {'regParam' : regParam, 'dim' : dim})
     
-    dsFactory = SVMLightDataSourceFactory("../../../../data/classification/skin_segmentation.dat", numberOfNodes, indices = 'roundRobin', shuffle = False)
+    # dsFactory = SVMLightDataSourceFactory("../../../../data/classification/skin_segmentation.dat", numberOfNodes,
+    # indices = 'roundRobin', shuffle = False)
+
+    dsFactory = FileDataSourceFactory(
+        filename="../../../../data/HIGGS/HIGGS.csv",
+        decoder=HIGGSDecoder(), numberOfNodes=numberOfNodes, indices='roundRobin', shuffle=False, cache=False)
+
     stoppingCriterion = MaxAmountExamples(6000)
         
-    aggregator = RadonPoint()
+    aggregator = Average()
     sync = AggregationAtTheEnd()
     
     exp = Experiment(executionMode = 'cpu', messengerHost = messengerHost, messengerPort = messengerPort, 
         numberOfNodes = numberOfNodes, sync = sync, 
         aggregator = aggregator, learnerFactory = learnerFactory, 
         dataSourceFactory = dsFactory, stoppingCriterion = stoppingCriterion, sleepTime=0)
-    exp.run("Linear_SVC_radon_point")
+    exp.run("Linear_SVC" + "_" + aggregator.__str__())
+
