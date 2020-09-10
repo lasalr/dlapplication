099be575fe8bc16b1d1ed8d12f4325b0dfcb3c3e
Lasal Ranasinghe <lran0009@student.monash.edu>
2020-09-10 21:33:59 +1000

    Removed HIGGs and SUSY dataDecoders. Left class for now. Using CSVDecoder in experiment

--- environments/datasources/dataDecoders/otherDataDecoders.py
+++ environments/datasources/dataDecoders/otherDataDecoders.py
@@ -1,35 +1,15 @@
-import numpy as np
-from environments.datasources.dataDecoders import DataDecoder, CSVDecoder
+from environments.datasources.dataDecoders import CSVDecoder
 
 
 class HIGGSDecoder(CSVDecoder):
     """
     Decoder for the HIGGS dataset. More info about dataset at https://archive.ics.uci.edu/ml/datasets/HIGGS
     """
-    def __init__(self, delimiter=',', labelCol=0):
-        CSVDecoder.__init__(self, delimiter, labelCol)
+    pass
 
-    def __call__(self, line):
-        parsed_line = [float(c) for c in line.split(self._delimiter)]
-        features = np.asarray(parsed_line[1:], dtype='float32')
-        label = int(parsed_line[self._labelCol])
-        return features, label
-
-    def __str__(self):
-        return "HIGGS from csv file"
 
 class SUSYDecoder(CSVDecoder):
     """
     Decoder for the SUSY dataset. More info about dataset at https://archive.ics.uci.edu/ml/datasets/SUSY
     """
-    def __init__(self, delimiter=',', labelCol=0):
-        CSVDecoder.__init__(self, delimiter, labelCol)
-
-    def __call__(self, line):
-        parsed_line = [float(c) for c in line.split(self._delimiter)]
-        features = np.asarray(parsed_line[1:], dtype='float32')
-        label = int(parsed_line[self._labelCol])
-        return features, label
-
-    def __str__(self):
-        return "HIGGS from csv file"
+    pass
