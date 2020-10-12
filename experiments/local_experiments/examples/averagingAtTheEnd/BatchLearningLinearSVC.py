e4d63b636f8b2ac3d6e19155367c4c3c31f8be42
Lasal Ranasinghe <lran0009@student.monash.edu>
2020-10-12 12:58:19 +1100

    Changed to run multiple experiments with values picked from lists for node_counts, learners, regParams,                                                                                aggregators, max_example_values

--- experiments/local_experiments/examples/averagingAtTheEnd/BatchLearningLinearSVC.py
+++ experiments/local_experiments/examples/averagingAtTheEnd/BatchLearningLinearSVC.py
@@ -10,25 +10,33 @@ from DLplatform.aggregating import Average
 from DLplatform.aggregating import RadonPoint
 from DLplatform.synchronizing.aggAtTheEnd import AggregationAtTheEnd
 from DLplatform.learning.factories.sklearnBatchLearnerFactory import SklearnBatchLearnerFactory
-from DLplatform.learning.batch.sklearnClassifiers import LogisticRegression, LinearSVC, LinearSVCRandomFF, LinearSVCNystrom
+from DLplatform.learning.batch.sklearnClassifiers import LogisticRegression, LinearSVC, LinearSVCRandomFF, \
+    LinearSVCNystrom
 from DLplatform.stopping import MaxAmountExamples
 from DLplatform.dataprovisioning import BatchDataScheduler, IntervalDataScheduler
+from itertools import product
 
 LOG_CONSOLE = True
 
 if __name__ == "__main__":
 
-    try:
+    messengerHost = 'localhost'
+    messengerPort = 5672
+    node_counts = [x for x in range(20, 81, 10)]
+    learners = [LinearSVC, LinearSVCRandomFF]
+    regParams = [0.1, 0.01, 0.001, 0.0001]
+    aggregators = [RadonPoint(), Average()]
+    max_example_values = [x for x in range(10000, 100001, 10000)]
 
-        messengerHost = 'localhost'
-        messengerPort = 5672
-        numberOfNodes = 100
+    # dim = 4 #skin_segmentation has 4 attributes
+    dim = 18  # SUSY has 18 features
+    # dim = 28  # HIGGS has 28 features
 
-        regParam = 0.001
-        # dim = 4 #skin_segmentation has 4 attributes
-        dim = 18  # SUSY has 18 features
-        # dim = 28  # HIGGS has 28 features
-        learnerFactory = SklearnBatchLearnerFactory(LinearSVC, {'regParam': regParam, 'dim': dim})
+    sync = AggregationAtTheEnd()
+    for (node_count, learner, regParam, aggregator, max_example_value) in product(node_counts, learners, regParams,
+                                                                                  aggregators, max_example_values):
+
+        numberOfNodes = node_count
 
         # dsFactory = SVMLightDataSourceFactory("../../../../data/classification/skin_segmentation.dat", numberOfNodes,
         # indices = 'roundRobin', shuffle = False)
@@ -43,28 +51,32 @@ if __name__ == "__main__":
         #     decoder=CSVDecoder(delimiter=',', labelCol=0), numberOfNodes=numberOfNodes, indices='roundRobin',
         #     shuffle=False, cache=False)
 
-        stoppingCriterion = MaxAmountExamples(20000)
-        aggregator = RadonPoint()
-        sync = AggregationAtTheEnd()
+        regParam = regParam
+        learnerFactory = SklearnBatchLearnerFactory(learner, {'regParam': regParam, 'dim': dim})
 
-        exp = Experiment(executionMode='cpu', messengerHost=messengerHost, messengerPort=messengerPort,
-                         numberOfNodes=numberOfNodes, sync=sync, aggregator=aggregator, learnerFactory=learnerFactory,
-                         dataSourceFactory=dsFactory, stoppingCriterion=stoppingCriterion, sleepTime=0.2,
-                         dataScheduler=BatchDataScheduler, minStartNodes=numberOfNodes, minStopNodes=numberOfNodes)
+        stoppingCriterion = MaxAmountExamples(max_example_value)
+        aggregator = aggregator
 
-        exp.run("Linear_SVC" + "_" + aggregator.__str__())
-        # exp.run("Linear_SVC")
+        try:
 
-    finally:
-        # Set console output to file at src. Below code will copy and rename the file
-        # to include a timestamp
-        if LOG_CONSOLE:
-            import os
-            import shutil
-            import datetime
+            exp = Experiment(executionMode='cpu', messengerHost=messengerHost, messengerPort=messengerPort,
+                             numberOfNodes=numberOfNodes, sync=sync, aggregator=aggregator,
+                             learnerFactory=learnerFactory,
+                             dataSourceFactory=dsFactory, stoppingCriterion=stoppingCriterion, sleepTime=0.2,
+                             dataScheduler=BatchDataScheduler, minStartNodes=numberOfNodes, minStopNodes=numberOfNodes)
 
-            src = '../../../../../Console Logs/console_logs.txt'
-            dst = './Results/' + 'console_logs_' + str(datetime.datetime.now()).replace(':', '_') + '.txt'
-            shutil.copyfile(src, dst)
-            # if os.path.isfile(path=src) and os.path.isfile(path=dst):
-            #     os.remove(path=src)
+            exp.run(learner.__name__ + '_' + aggregator.__str__())
+
+        finally:
+            # Set console output to file at src. Below code will copy and rename the file
+            # to include a timestamp
+            if LOG_CONSOLE:
+                import os
+                import shutil
+                import datetime
+                # TODO change code to create separate files
+                src = '../../../../../Console Logs/console_logs.txt'
+                dst = './Results/' + 'console_logs_' + str(datetime.datetime.now()).replace(':', '_') + '.txt'
+                shutil.copyfile(src, dst)
+                # if os.path.isfile(path=src) and os.path.isfile(path=dst):
+                #     os.remove(path=src)
