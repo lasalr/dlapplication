e57b6cbd5c775dbd562e5ebec357917d3f3e6b89
Lasal Ranasinghe <lran0009@student.monash.edu>
2020-10-16 23:45:43 +1100

    Created file for RFF experiments

---
+++ experiments/local_experiments/RFF_experiments/RFFLearningExperiments.py
@@ -0,0 +1,29 @@
+import sys
+import os
+sys.path.append("../../../../dlapplication")
+sys.path.append("../../../../dlplatform")
+
+from split_data import split_dataset
+from environments.local_environment import Experiment
+from environments.datasources.standardDataSourceFactories import FileDataSourceFactory, SVMLightDataSourceFactory
+from environments.datasources.dataDecoders.otherDataDecoders import CSVDecoder
+
+from DLplatform.aggregating import Average
+from DLplatform.aggregating import RadonPoint
+from DLplatform.synchronizing.aggAtTheEnd import AggregationAtTheEnd
+from DLplatform.learning.factories.sklearnBatchLearnerFactory import SklearnBatchLearnerFactory
+from DLplatform.learning.batch.sklearnClassifiers import LogisticRegression, LinearSVC, LinearSVCRandomFF, \
+    LinearSVCNystrom
+
+from DLplatform.stopping import MaxAmountExamples
+from DLplatform.dataprovisioning import BatchDataScheduler, IntervalDataScheduler
+
+from itertools import product
+from datetime import datetime
+import numpy as np
+
+LOG_CONSOLE = True
+
+if __name__ == '__main__':
+    pass
+
