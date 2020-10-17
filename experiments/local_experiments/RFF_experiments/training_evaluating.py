bcdfd05ed3e977ee23c6c6b6ec7a2135ca5b6b52
Lasal Ranasinghe <lran0009@student.monash.edu>
2020-10-17 17:47:43 +1100

    Added scaling option to linear svc and kernel svm

--- experiments/local_experiments/RFF_experiments/training_evaluating.py
+++ experiments/local_experiments/RFF_experiments/training_evaluating.py
@@ -1,6 +1,8 @@
 from sklearn.metrics import roc_auc_score
 from scipy.spatial.distance import pdist
 from sklearn.kernel_approximation import RBFSampler
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
 from sklearn.svm import LinearSVC
 from sklearn.svm import SVC
 import numpy as np
@@ -8,19 +10,31 @@ import numpy as np
 RANDOM_STATE = 123
 
 
-def train_rff_linear_svc(X, y, c, sampler: RBFSampler = None):
+def train_rff_linear_svc(X, y, c, sampler: RBFSampler = None, scale=False):
+    model = LinearSVC(C=c, max_iter=500, dual=False, random_state=RANDOM_STATE)
+
     if sampler is not None:
         X = sampler.fit_transform(X)
 
-    model = LinearSVC(C=c, max_iter=500, dual=False, random_state=RANDOM_STATE)
-    model.fit(X, y)
-    return model
+    if scale:
+        model_pipeline = make_pipeline(StandardScaler(), model)
+        model_pipeline.fit(X, y)
+        return model_pipeline
+    else:
+        model.fit(X, y)
+        return model
 
 
-def train_rff_kernel_svm(X, y, c):
-    model = SVC(kernel='rbf', gamma='scale', C=c, max_iter=500, random_state=RANDOM_STATE)
-    model.fit(X, y)
-    return model
+def train_rff_kernel_svm(X, y, c, scale=False):
+    model = SVC(kernel='rbf', gamma='auto', C=c, max_iter=1000, random_state=RANDOM_STATE)
+
+    if scale:
+        model_pipeline = make_pipeline(StandardScaler(), model)
+        model_pipeline.fit(X, y)
+        return model_pipeline
+    else:
+        model.fit(X, y)
+        return model
 
 
 def gamma_estimate(features, n=100):
