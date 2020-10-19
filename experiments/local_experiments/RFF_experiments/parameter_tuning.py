d0bb3f649c60b2dd9d35097646d2b395db5a7c62
Lasal Ranasinghe <lran0009@student.monash.edu>
2020-10-19 22:44:15 +1100

    Changed some settings Temporarily commented out LinearSVC (without RFF) for debugging LinearSVC with RFF

--- experiments/local_experiments/RFF_experiments/parameter_tuning.py
+++ experiments/local_experiments/RFF_experiments/parameter_tuning.py
@@ -26,25 +26,26 @@ if __name__ == '__main__':
     print('Data loaded')
 
     # Parameter tuning for Linear SVC without RFF
-    print('Starting: Parameter tuning for Linear SVC without RFF...')
-    param_grid = {'C': [0.00001, 0.00005, 0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 5, 10, 50, 100],
-                  'dual': [True, False], 'random_state': [RANDOM_STATE]}
+    # print('Starting: Parameter tuning for Linear SVC without RFF...')
+    # param_grid = {'C': [0.00001, 0.00005, 0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 5, 10, 50, 100],
+    #               'dual': [True, False], 'random_state': [RANDOM_STATE]}
+    #
+    # gs_model = GridSearchCV(estimator=LinearSVC(), verbose=1, param_grid=param_grid, scoring=roc_auc_scorer, n_jobs=-1)
+    # gs_model.fit(X, y)
+    # write_csv(path='./Results/', name='param_tune_linearsvc_', start_time=start_time,
+    #           results=gs_model.cv_results_, sortby_col='rank_test_score')
 
-    gs_model = GridSearchCV(estimator=LinearSVC(), verbose=1, param_grid=param_grid, scoring=roc_auc_scorer, n_jobs=-1)
-    gs_model.fit(X, y)
-    write_csv(path='./Results/', name='param_tune_linearsvc_', start_time=start_time,
-              results=gs_model.cv_results_, sortby_col='rank_test_score')
-
-    # # Parameter tuning for Linear SVC with RFF
+    # Parameter tuning for Linear SVC with RFF
     gamma_initial = 0.005
     param_grid_rff = {'C': [0.00001, 0.00005, 0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 5, 10, 50, 100],
-                  'dual': [True, False], 'random_state': [RANDOM_STATE],
-                  'rff_sampler_gamma': [gamma_initial, gamma_initial*0.90, gamma_initial*0.80, gamma_initial*0.60,
-                                        gamma_initial*0.30, gamma_initial*0.10, gamma_initial*0.05, gamma_initial*0.01],
-                  'rff_sampler_n_components': [29]}
+                      'dual': (True, False), 'random_state': [RANDOM_STATE],
+                      'rff_sampler_gamma': [gamma_initial, gamma_initial * 0.90, gamma_initial * 0.80,
+                                            gamma_initial * 0.60, gamma_initial * 0.30, gamma_initial * 0.10,
+                                            gamma_initial * 0.05, gamma_initial * 0.01],
+                      'rff_sampler_n_components': [29]}
 
-    gs_model_rff = GridSearchCV(estimator=LinearSVCSampledRFF(), verbose=1, param_grid=param_grid_rff,
-                            scoring=roc_auc_scorer, n_jobs=-1)
+    gs_model_rff = GridSearchCV(estimator=LinearSVCSampledRFF(), verbose=2, param_grid=param_grid_rff,
+                                scoring=roc_auc_scorer, n_jobs=-1)
     gs_model_rff.fit(X, y)
     write_csv(path='./Results/', name='param_tune_linearsvc_rff_', start_time=start_time,
               results=gs_model_rff.cv_results_, sortby_col='rank_test_score')
