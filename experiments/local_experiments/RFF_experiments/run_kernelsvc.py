3ba63f331cc98abc0072e5b9cccd86f809403b0a
Lasal Ranasinghe <lran0009@student.monash.edu>
2020-10-22 18:13:25 +1100

    Changed settings for gridsearch

--- experiments/local_experiments/RFF_experiments/run_kernelsvc.py
+++ experiments/local_experiments/RFF_experiments/run_kernelsvc.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV, train_test_split
 from datetime import datetime
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
@@ -21,23 +21,30 @@ if __name__ == '__main__':
     file_path = '../../../data/SUSY/SUSY.csv'
     data_label_col = 0
     validation_file_path = os.path.join(os.path.dirname(file_path), 'split', 'VAL_' + os.path.basename(file_path))
-
+    tune_data_fraction = 0.20
     print('Splitting dataset...')
     split_dataset(file_path=file_path)  # Does not save if file is present
     X, y = load_data(path=validation_file_path, label_col=data_label_col, d=dim)
-    print('Data loaded')
-
+    X_other, X_param, y_other, y_param = train_test_split(X, y, test_size=tune_data_fraction, random_state=RANDOM_STATE)
+    print('{}% of validation data loaded for GridSearchCV'.format(tune_data_fraction * 100))
+    print('X_param={}\ny_param.shape={}'.format(X_param.shape, y_param.shape))
     pipe = Pipeline([('scaler', StandardScaler()), ('svc', SVC())])
 
+    # param_grid = {
+    #     'svc__C': [2 ** x for x in np.linspace(250, 260, 5)],
+    #     'svc__gamma': [2 ** x for x in range(-11, 2)],
+    #     'svc__random_state': [RANDOM_STATE],
+    #     'svc__decision_function_shape': ['ovo']}
+
     param_grid = {
-        'svc__C': [2 ** x for x in np.linspace(250, 260, 5)],
-        'svc__gamma': [2 ** x for x in range(-11, 2)],
+        'svc__C': [252],
+        'svc__gamma': [0.00390625],
         'svc__random_state': [RANDOM_STATE],
         'svc__decision_function_shape': ['ovo']}
 
     gs_model_kernelsvm = GridSearchCV(estimator=pipe, param_grid=param_grid, cv=5, verbose=1, scoring='roc_auc',
-                                      n_jobs=-1)
-    gs_model_kernelsvm.fit(X, y)
+                                      n_jobs=5)
+    gs_model_kernelsvm.fit(X_param, y_param)
 
     print('writing results to file...')
     write_csv(path='./Results/', name='param_tune_kernelsvc_', start_time=start_time,
