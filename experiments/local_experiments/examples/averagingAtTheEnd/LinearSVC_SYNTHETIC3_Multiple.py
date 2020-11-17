1a1084af488acd36aa24d1855db1d8f7ca2bc3c2
Lasal Ranasinghe <lran0009@student.monash.edu>
2020-11-17 22:49:28 +1100

    Changed setting to achieve h=1

--- experiments/local_experiments/examples/averagingAtTheEnd/LinearSVC_SYNTHETIC3_Multiple.py
+++ experiments/local_experiments/examples/averagingAtTheEnd/LinearSVC_SYNTHETIC3_Multiple.py
@@ -26,9 +26,9 @@ if __name__ == "__main__":
     max_example_list = [2000, 1000, 500, 200, 100, 50, 25]
 
     dim = 5  # SYNTHETIC1 has 5 features
-    numberOfNodes = 196  # 205
-    n_components = 11  # 202
-    coord_sleep_time = 0.3  # 7
+    numberOfNodes = 205
+    n_components = 202
+    coord_sleep_time = 0.4  # 7
     learner = LinearSVCRandomFF
     regParam = 512
     gamma = 0.0078125
